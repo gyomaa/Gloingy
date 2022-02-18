2280d494dfe327623bb47dc8a888ea1107a3be33
gyomaa <kaybsaeng@gmail.com>
2022-02-18 14:03:14 -0800

    SUCCESS

---
+++ experiment/Assets/scripts/MoveScene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MoveScene : MonoBehaviour
+{
+    [SerializeField] private string loadlevel;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(loadlevel);
+        }
+    }
+
+}
