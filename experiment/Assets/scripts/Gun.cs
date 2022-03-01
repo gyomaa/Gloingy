b58258d1012c916d5795d71153b0812e6fac45d5
gyomaa <kaybsaeng@gmail.com>
2022-03-01 14:03:43 -0800

    GUNS FINISHED

---
+++ experiment/Assets/scripts/Gun.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 15f;
+
+    public Camera fpsCam;
+    private float nextTimeToFire = 0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+    }
+
+
+    // Mechanic for gun shooting
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+
+}
